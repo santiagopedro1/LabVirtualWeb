fc9188840c4aa96385e640e6ebdbac7e7ff04c79
Pedro Santiago <pedrossantiago1@gmail.com>
2022-01-25 22:01:38 -0300

    Teste

--- js/echarts-realtime.js
+++ js/echarts-realtime.js
@@ -1,3 +1,41 @@
+const myChart = echarts.init(document.getElementById('chart'), 'echarts-theme', { renderer: 'svg' })
+
+fetch(`https://labvirtualapi.loca.lt/?_id=${new Date().toLocaleDateString('pt-BR')}`, { method: 'GET', mode: 'cors', credentials: 'omit' }).then(
+	(res) => {
+		return res.json().then((dados) => {
+			let option = {
+				title: {
+					text: `Gráfico dia ${new Date().toLocaleDateString('pt-BR')}`,
+				},
+				tooltip: {
+					trigger: 'axis',
+				},
+				xAxis: {
+					type: 'category',
+					boundaryGap: false,
+					data: dados.hora,
+				},
+				yAxis: {
+					type: 'value',
+				},
+				series: [
+					{
+						name: 'A',
+						type: 'line',
+						data: dados.distanciaA,
+					},
+					{
+						name: 'B',
+						type: 'line',
+						data: dados.distanciaB,
+					},
+				],
+			}
+			myChart.setOption(option)
+		})
+	}
+)
+
 const getData = () => {
 	var d = new Date()
 	h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes() - (d.getMinutes() % 15) + 15, 0, 0)
@@ -7,43 +45,27 @@ const getData = () => {
 	fetch(`https://labvirtualapi.loca.lt/?_id=${new Date().toLocaleDateString('pt-BR')}`, { method: 'GET', mode: 'cors', credentials: 'omit' }).then(
 		(res) => {
 			return res.json().then((dados) => {
-				let option = {
-					title: {
-						text: `Gráfico dia ${new Date().toLocaleDateString('pt-BR')}`,
-					},
-					tooltip: {
-						trigger: 'axis',
-					},
+				myChart.setOption({
 					xAxis: {
-						type: 'category',
-						boundaryGap: false,
 						data: dados.hora,
-					},
-					yAxis: {
-						type: 'value',
 					},
 					series: [
 						{
 							name: 'A',
-							type: 'line',
 							data: dados.distanciaA,
 						},
 						{
 							name: 'B',
-							type: 'line',
 							data: dados.distanciaB,
 						},
 					],
-				}
-
-				window.addEventListener('resize', () => {
-					myChart.resize()
 				})
-
-				let myChart = echarts.init(document.getElementById('chart'), 'echarts-theme', { renderer: 'svg' })
-				myChart.setOption(option)
 				console.log(new Date().toLocaleTimeString('pt-BR') + ' cheguei')
 			})
 		}
 	)
 }
+
+window.addEventListener('resize', () => {
+	myChart.resize()
+})
