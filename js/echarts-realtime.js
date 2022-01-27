820669883d1aeee972981e75bea9d76954206179
Pedro Santiago <pedrossantiago1@gmail.com>
2022-01-27 11:30:47 -0300

    Teste

--- js/echarts-realtime.js
+++ js/echarts-realtime.js
@@ -1,42 +1,38 @@
 const myChart = echarts.init(document.getElementById('chart'), 'echarts-theme', { renderer: 'svg' })
 
-fetch(`https://labvirtualapi.loca.lt/?_id=${new Date().toLocaleDateString('pt-BR')}`, { method: 'GET' }).then((res) => {
-	return res.json().then((dados) => {
-		let option = {
-			title: {
-				text: `Gráfico dia ${new Date().toLocaleDateString('pt-BR')}`,
-			},
-			tooltip: {
-				trigger: 'axis',
-			},
-			xAxis: {
-				type: 'category',
-				boundaryGap: false,
-				data: dados.hora,
-			},
-			yAxis: {
-				type: 'value',
-			},
-			series: [
-				{
-					name: 'A',
-					type: 'line',
-					data: dados.distanciaA,
-				},
-				{
-					name: 'B',
-					type: 'line',
-					data: dados.distanciaB,
-				},
-			],
-		}
-		myChart.setOption(option)
-	})
-})
+let option = {
+	title: {
+		text: `Gráfico dia ${new Date().toLocaleDateString('pt-BR')}`,
+	},
+	tooltip: {
+		trigger: 'axis',
+	},
+	xAxis: {
+		type: 'category',
+		boundaryGap: false,
+		data: [],
+	},
+	yAxis: {
+		type: 'value',
+	},
+	series: [
+		{
+			name: 'A',
+			type: 'line',
+			data: [],
+		},
+		{
+			name: 'B',
+			type: 'line',
+			data: [],
+		},
+	],
+}
+myChart.setOption(option)
 
 const getData = () => {
 	var d = new Date()
-	h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes() - (d.getMinutes() % 15) + 15, 0, 0)
+	h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes() - (d.getMinutes() % 15) + 15, 3, 0)
 	e = h - d
 	window.setTimeout(getData, e)
 
