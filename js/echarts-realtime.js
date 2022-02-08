a6f21c8c782e4bda8b01595a8e2f215a61d9d77f
Pedro Santiago <pedrossantiago1@gmail.com>
2022-02-08 12:26:57 -0300

    Consulte o arquivo README para uma lista de mudanças

--- js/echarts-realtime.js
+++ js/echarts-realtime.js
@@ -40,29 +40,27 @@ const getData = () => {
 	e = h - d
 	window.setTimeout(getData, e)
 
-	fetch(`https://apilabvirtual.loca.lt/?_id=${new Date().toLocaleDateString('pt-BR')}`, { method: 'GET', mode: 'no-cors', headers: header }).then(
-		(res) => {
-			return res.json().then((dados) => {
-				myChart.setOption({
-					xAxis: {
-						data: dados.hora,
+	fetch(`https://apilabvirtual.loca.lt/?_id=${new Date().toLocaleDateString('pt-BR')}`, { method: 'GET', headers: header }).then((res) => {
+		return res.json().then((dados) => {
+			myChart.setOption({
+				xAxis: {
+					data: dados.hora,
+				},
+				series: [
+					{
+						name: 'A',
+						data: dados.distanciaA,
 					},
-					series: [
-						{
-							name: 'A',
-							data: dados.distanciaA,
-						},
-						{
-							name: 'B',
-							data: dados.distanciaB,
-						},
-					],
-				})
-				let x = new Date()
-				console.log(`cheguei ${x.getHours()}:${x.getMinutes()}:${x.getSeconds()}.${x.getMilliseconds()}`)
+					{
+						name: 'B',
+						data: dados.distanciaB,
+					},
+				],
 			})
-		}
-	)
+			let x = new Date()
+			console.log(`cheguei ${x.getHours()}:${x.getMinutes()}:${x.getSeconds()}.${x.getMilliseconds()}`)
+		})
+	})
 }
 
 window.addEventListener('resize', () => {
