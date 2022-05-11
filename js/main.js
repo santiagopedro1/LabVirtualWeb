0d840d6746798380087371ef71e18fde475bacc6
Pedro Santiago <pedrossantiago1@gmail.com>
2022-05-11 17:38:58 -0300

    Refatoração de código

--- js/main.js
+++ js/main.js
@@ -1,59 +1,4 @@
 let myChart = echarts.init(document.getElementById('chart'), 'echarts-theme', { renderer: 'svg' })
-document.getElementById('chart').style.display = 'none'
-let option = {
-	tooltip: {
-		trigger: 'axis',
-	},
-	toolbox: {
-		right: 60,
-		show: true,
-	},
-	xAxis: {
-		type: 'category',
-		boundaryGap: false,
-		data: [],
-	},
-	yAxis: {
-		type: 'value',
-	},
-	legend: {
-		show: false,
-	},
-	series: [
-		{
-			name: 'Umidade do sensor A',
-			type: 'line',
-			data: [],
-		},
-		{
-			name: 'Umidade do sensor B',
-			type: 'line',
-			data: [],
-		},
-		{
-			name: 'Conductividade do sensor A',
-			type: 'line',
-			data: [],
-		},
-		{
-			name: 'Conductividade do sensor B',
-			type: 'line',
-			data: [],
-		},
-		{
-			name: 'Temperatura do sensor A',
-			type: 'line',
-			data: [],
-		},
-
-		{
-			name: 'Temperatura do sensor B',
-			type: 'line',
-			data: [],
-		},
-	],
-}
-myChart.setOption(option)
 
 window.addEventListener('resize', () => {
 	myChart.resize()
@@ -83,7 +28,6 @@ function trigger(data) {
 }
 
 function carregarDados(data) {
-	document.getElementById('chart').style.display = 'block'
 	myChart.showLoading({
 		text: 'CARREGANDO DADOS',
 		color: '#32c6efff',
@@ -94,45 +38,69 @@ function carregarDados(data) {
 
 	fetch(`https://labvirtual-api.vercel.app/api/${data}`, { method: 'GET', mode: 'cors' }).then((res) => {
 		return res.json().then((dados) => {
-			myChart.setOption({
-				title: {
-					text: `Gráfico do dia ${dados._id}`,
-				},
-				xAxis: {
-					data: dados.hora,
-				},
-				series: [
-					{
-						name: 'Umidade do sensor A',
-						data: dados.sensor.A.Umidade,
-					},
-					{
-						name: 'Umidade do sensor B',
-						data: dados.sensor.B.Umidade,
-					},
-					{
-						name: 'Conductividade do sensor A',
-						data: dados.sensor.A.Conductividade,
-					},
-					{
-						name: 'Conductividade do sensor B',
-						data: dados.sensor.B.Conductividade,
-					},
-					{
-						name: 'Temperatura do sensor A',
-						data: dados.sensor.A.Temperatura,
-					},
-					{
-						name: 'Temperatura do sensor B',
-						data: dados.sensor.B.Temperatura,
-					},
-				],
-			})
-			myChart.hideLoading()
+			criarGrafico(dados)
 		})
 	})
 }
 
-$(function () {
-	rome(input, { time: false, max: new Date(), inputFormat: 'DD/MM/YYYY' })
-})
+//functions
+
+//criar gráfico
+function criarGrafico(dados) {
+	myChart.setOption({
+		tooltip: {
+			trigger: 'axis',
+		},
+		toolbox: {
+			right: 60,
+			show: true,
+		},
+		legend: {
+			show: false,
+		},
+		title: {
+			text: `Gráfico do dia ${dados._id}`,
+		},
+		xAxis: {
+			type: 'category',
+			boundaryGap: false,
+			data: dados.hora,
+		},
+		yAxis: {
+			type: 'value',
+		},
+		series: [
+			{
+				name: 'Umidade do sensor A',
+				type: 'line',
+				data: dados.sensor.A.Umidade,
+			},
+			{
+				name: 'Umidade do sensor B',
+				type: 'line',
+				data: dados.sensor.B.Umidade,
+			},
+			{
+				name: 'Conductividade do sensor A',
+				type: 'line',
+				data: dados.sensor.A.Conductividade,
+			},
+			{
+				name: 'Conductividade do sensor B',
+				type: 'line',
+				data: dados.sensor.B.Conductividade,
+			},
+			{
+				name: 'Temperatura do sensor A',
+				type: 'line',
+				data: dados.sensor.A.Temperatura,
+			},
+			{
+				name: 'Temperatura do sensor B',
+				type: 'line',
+				data: dados.sensor.B.Temperatura,
+			},
+		],
+	})
+	myChart.hideLoading()
+}
