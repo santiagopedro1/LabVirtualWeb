78d1cad683a481ab6a5f5dade3f8f2e05d53b413
Pedro Santiago <pedrossantiago1@gmail.com>
2023-03-11 09:51:46 -0300

    De volta para tabsize 4

--- src/routes/api/leituras/+server.ts
+++ src/routes/api/leituras/+server.ts
@@ -4,45 +4,45 @@ import { getDateForQuery, getDateObj } from '$lib/dateUtils'
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async ({ request, url }) => {
-  const headers = request.headers
-  if (headers.get('key') !== '123')
-    return new Response(JSON.stringify({ message: 'no key' }), {
-      status: 403
+    const headers = request.headers
+    if (headers.get('key') !== '123')
+        return new Response(JSON.stringify({ message: 'no key' }), {
+            status: 403
+        })
+
+    const date = url.searchParams.get('date')
+    if (!date)
+        return new Response(JSON.stringify({ message: 'no date' }), {
+            status: 400
+        })
+
+    const dateObj = getDateObj(date)
+    if (!dateObj)
+        return new Response(JSON.stringify({ message: 'invalid date' }), {
+            status: 400
+        })
+
+    const { inicioDia, fimDia } = getDateForQuery(dateObj)
+
+    const leituras = await prisma.leituras_de_sensor.findMany({
+        where: {
+            data_hora: {
+                gte: inicioDia,
+                lt: fimDia
+            }
+        },
+        orderBy: {
+            data_hora: 'asc'
+        }
     })
 
-  const date = url.searchParams.get('date')
-  if (!date)
-    return new Response(JSON.stringify({ message: 'no date' }), {
-      status: 400
-    })
-
-  const dateObj = getDateObj(date)
-  if (!dateObj)
-    return new Response(JSON.stringify({ message: 'invalid date' }), {
-      status: 400
-    })
-
-  const { inicioDia, fimDia } = getDateForQuery(dateObj)
-
-  const leituras = await prisma.leituras_de_sensor.findMany({
-    where: {
-      data_hora: {
-        gte: inicioDia,
-        lt: fimDia
-      }
-    },
-    orderBy: {
-      data_hora: 'asc'
-    }
-  })
-
-  if (leituras.length === 0) return new Response(null, { status: 404 })
-  else
-    return new Response(
-      JSON.stringify({
-        displayDate: dateObj.toLocaleDateString('pt-BR'),
-        leituras: leituras
-      }),
-      { status: 200 }
-    )
+    if (leituras.length === 0) return new Response(null, { status: 404 })
+    else
+        return new Response(
+            JSON.stringify({
+                displayDate: dateObj.toLocaleDateString('pt-BR'),
+                leituras: leituras
+            }),
+            { status: 200 }
+        )
 }
--- src/routes/dados/+page.svelte
+++ src/routes/dados/+page.svelte
@@ -1,119 +1,119 @@
 <script lang="ts">
-  import datepicker from '$lib/datepicker'
+    import datepicker from '$lib/datepicker'
 
-  import Modal from '$components/modal.svelte'
+    import Modal from '$components/modal.svelte'
 
-  import { page } from '$app/stores'
-  import { onMount } from 'svelte'
+    import { page } from '$app/stores'
+    import { onMount } from 'svelte'
 
-  import { getDateObj } from '$lib/dateUtils'
+    import { getDateObj } from '$lib/dateUtils'
 
-  //-----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------
 
-  type Leituras_de_sensor = {
-    id_sensor_de_usuario: number
-    data_hora: string
-    leitura: {
-      Condutividade: number
-      Temperatura: number
-      Umidade_gravimetrica: number
-    }
-  }
-  let LineChart: any
-  let selectedDate: Date
-  let dados: {
-    leituras: Leituras_de_sensor[]
-    displayDate: string
-  }
-  const modal = {
-    title: '',
-    message: '',
-    isOpen: false
-  }
-
-  if ($page.url.searchParams.has('data')) {
-    const date = $page.url.searchParams.get('data')!
-    const parsedDate = getDateObj(date)
-    if (parsedDate) selectedDate = parsedDate
-  }
-
-  async function fetchLeituras(data: Date) {
-    console.log('Buscando dados da API')
-    fetch(`/api/leituras?date=${data.toLocaleDateString('pt-BR')}`, {
-      headers: {
-        key: '123'
-      }
-    })
-      .then(async response => {
-        switch (response.status) {
-          case 200:
-            dados = await response.json()
-            break
-          default:
-            showModal(
-              'Nada encontrado',
-              'Não foram encontradas leituras para a data selecionada'
-            )
-            break
+    type Leituras_de_sensor = {
+        id_sensor_de_usuario: number
+        data_hora: string
+        leitura: {
+            Condutividade: number
+            Temperatura: number
+            Umidade_gravimetrica: number
         }
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }
+    }
+    let LineChart: any
+    let selectedDate: Date
+    let dados: {
+        leituras: Leituras_de_sensor[]
+        displayDate: string
+    }
+    const modal = {
+        title: '',
+        message: '',
+        isOpen: false
+    }
 
-  function showModal(title: string, message: string) {
-    modal.title = title
-    modal.message = message
-    modal.isOpen = true
-  }
+    if ($page.url.searchParams.has('data')) {
+        const date = $page.url.searchParams.get('data')!
+        const parsedDate = getDateObj(date)
+        if (parsedDate) selectedDate = parsedDate
+    }
 
-  onMount(async () => {
-    LineChart = (await import('$components/lineChart.svelte')).default
-    await import('$lib/css/datepicker.css')
-    datepicker(
-      (selectedDay: Date) => (selectedDate = selectedDay),
-      selectedDate
-    )
-  })
+    async function fetchLeituras(data: Date) {
+        console.log('Buscando dados da API')
+        fetch(`/api/leituras?date=${data.toLocaleDateString('pt-BR')}`, {
+            headers: {
+                key: '123'
+            }
+        })
+            .then(async response => {
+                switch (response.status) {
+                    case 200:
+                        dados = await response.json()
+                        break
+                    default:
+                        showModal(
+                            'Nada encontrado',
+                            'Não foram encontradas leituras para a data selecionada'
+                        )
+                        break
+                }
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
+    function showModal(title: string, message: string) {
+        modal.title = title
+        modal.message = message
+        modal.isOpen = true
+    }
+
+    onMount(async () => {
+        LineChart = (await import('$components/lineChart.svelte')).default
+        await import('$lib/css/datepicker.css')
+        datepicker(
+            (selectedDay: Date) => (selectedDate = selectedDay),
+            selectedDate
+        )
+    })
 </script>
 
 <svelte:head>
-  <title>Dados - Labvirtual</title>
+    <title>Dados - Labvirtual</title>
 </svelte:head>
 
 <section class="dark:text-white">
-  <p>Selecione uma data para ver os dados</p>
+    <p>Selecione uma data para ver os dados</p>
 
-  <section class="flex gap-6 items-center my-4">
-    <input
-      id="datepicker"
-      type="text"
-      readonly
-      placeholder="Pick a date"
-      class="select-none"
-    />
-    <button
-      class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-      on:click={() => fetchLeituras(selectedDate)}
-    >
-      VAI
-    </button>
-  </section>
+    <section class="flex gap-6 items-center my-4">
+        <input
+            id="datepicker"
+            type="text"
+            readonly
+            placeholder="Pick a date"
+            class="select-none"
+        />
+        <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+            on:click={() => fetchLeituras(selectedDate)}
+        >
+            GERAR GRÁFICO
+        </button>
+    </section>
 
-  {#if dados}
-    {#if dados.leituras.length > 0}
-      <svelte:component
-        this={LineChart}
-        data={dados.leituras}
-        displayDate={dados.displayDate}
-      />
+    {#if dados}
+        {#if dados.leituras.length > 0}
+            <svelte:component
+                this={LineChart}
+                data={dados.leituras}
+                displayDate={dados.displayDate}
+            />
+        {/if}
     {/if}
-  {/if}
 
-  <Modal
-    bind:isOpen={modal.isOpen}
-    title={modal.title}
-    message={modal.message}
-  />
+    <Modal
+        bind:isOpen={modal.isOpen}
+        title={modal.title}
+        message={modal.message}
+    />
 </section>
