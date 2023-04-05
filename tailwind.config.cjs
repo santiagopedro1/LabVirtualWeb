26a9d649c3959d9365fcefcde10e0ebec2ffeea4
Pedro Santiago <pedrossantiago1@gmail.com>
2023-04-05 19:40:38 -0300

    feat: tela de introducao do projeto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#019B43',
-        secondary: '#F7B100',
-        tertiary: '#BF1022'
-      },
-      fontFamily: {
-        'open-sans': ['Open Sans', 'sans-serif']
-      }
-    }
-  },
-  plugins: [],
-  darkMode: 'class'
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#019B43',
+                secondary: '#F7B100',
+                tertiary: '#BF1022'
+            },
+            fontFamily: {
+                'open-sans': ['Open Sans', 'sans-serif']
+            }
+        }
+    },
+    plugins: [require('@tailwindcss/typography')],
+    darkMode: 'class'
 }
