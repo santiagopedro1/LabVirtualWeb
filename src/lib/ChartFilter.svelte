a07de700dc813bf1ee2231672290899cfd19e7de
Pedro Santiago <pedrossantiago1@gmail.com>
2023-04-13 15:56:42 -0300

    fix: switch no mobile

--- src/lib/ChartFilter.svelte
+++ src/lib/ChartFilter.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+    export let sensores: SensorData[]
+    export let fn: (id: number, data: string) => void
+
     interface SensorData {
         id: number
         dataRead: string[]
         name: string
         description: string
     }
-
-    export let sensores: SensorData[]
-    export let fn: (id: number, data: string) => void
 </script>
 
 <div class="flex justify-around">
@@ -36,7 +36,7 @@
                                 }}
                             />
                             <div
-                                class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-secondary"
+                                class="w-11 h-6 relative bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-secondary"
                             />
                         </label>
                     </div>
