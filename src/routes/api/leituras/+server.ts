453092cab6887e20e5661a41f1536bb7269c910c
Pedro Santiago <pedrossantiago1@gmail.com>
2023-05-06 11:16:15 -0300

    download e grafico no mesmo api endpoint e usando forms

--- src/routes/api/leituras/+server.ts
+++ src/routes/api/leituras/+server.ts
@@ -1,3 +1,4 @@
+import { json } from '@sveltejs/kit'
 import { prisma } from '$lib/prisma'
 import { getDateForQuery, getDateObj, fixDate } from '$lib/dateUtils'
 
@@ -6,21 +7,31 @@ import type { RequestHandler } from './$types'
 export const GET: RequestHandler = async ({ request, url }) => {
     const headers = request.headers
     if (headers.get('key') !== '123')
-        return new Response(JSON.stringify({ message: 'no key' }), {
-            status: 403
-        })
+        return json(
+            { message: 'Sem chave de API' },
+            {
+                status: 403
+            }
+        )
 
-    const date = url.searchParams.get('date')
+    const date = url.searchParams.get('data')
+    const download = url.searchParams.get('download')
     if (!date)
-        return new Response(JSON.stringify({ message: 'no date' }), {
-            status: 400
-        })
+        return json(
+            { message: 'Sem data' },
+            {
+                status: 400
+            }
+        )
 
     const dateObj = getDateObj(date)
     if (!dateObj)
-        return new Response(JSON.stringify({ message: 'invalid date' }), {
-            status: 400
-        })
+        return json(
+            { message: 'Data inválida' },
+            {
+                status: 400
+            }
+        )
 
     const { inicioDia, fimDia } = getDateForQuery(dateObj)
 
@@ -61,11 +72,92 @@ export const GET: RequestHandler = async ({ request, url }) => {
         return acc
     }, {})
 
-    return new Response(
-        JSON.stringify({
-            displayDate: dateObj.toLocaleDateString('pt-BR'),
-            leituras
-        }),
-        { status: 200 }
-    )
+    if (!download)
+        return json(
+            {
+                displayDate: dateObj.toLocaleDateString('pt-BR'),
+                leituras
+            },
+            { status: 200 }
+        )
+    else {
+        const format = download
+
+        switch (format) {
+            default:
+                return json(null, { status: 400 })
+
+            case 'json':
+                const leiturasJsonFixed = fixDates(leituras)
+                return json(leiturasJsonFixed, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    status: 200
+                })
+
+            case 'csv':
+                return json(JSONtoCSV(leituras), {
+                    headers: {
+                        'Content-Type': 'text/csv'
+                    },
+                    status: 200
+                })
+        }
+    }
+}
+
+function JSONtoCSV(dados: Leitura): string {
+    const headers = new Set<string>()
+    const rows: Record<string, string | number>[] = []
+
+    Object.keys(dados).forEach(sensor => {
+        Object.values(dados[sensor]).forEach((leitura, index) => {
+            if (!rows[index]) rows[index] = {}
+            Object.keys(leitura).forEach(key => {
+                if (key === 'data_hora') {
+                    headers.add('horario')
+                    const date = new Date(leitura[key])
+                    const hora =
+                        date.getHours() < 10
+                            ? `0${date.getHours()}`
+                            : date.getHours()
+                    const minuto =
+                        date.getMinutes() < 10
+                            ? `0${date.getMinutes()}`
+                            : date.getMinutes()
+                    const horario = `${hora}:${minuto}`
+                    key = 'horario'
+                    rows[index][key] = horario
+                } else {
+                    headers.add(`${sensor}/${key}`)
+                    rows[index][`${sensor}/${key}`] = leitura[key]
+                }
+            })
+        })
+    })
+
+    const csv =
+        Array.from(headers).join(',') +
+        '\n' +
+        rows
+            .map(row =>
+                Array.from(headers)
+                    .map(header => String(row[header]))
+                    .join(',')
+            )
+            .join('\n')
+
+    return csv
+}
+
+function fixDates(dados: Leitura): Leitura {
+    Object.keys(dados).forEach(sensor => {
+        Object.values(dados[sensor]).forEach(leitura => {
+            const newDate = new Date(leitura.data_hora)
+            newDate.setHours(newDate.getHours() - 3)
+            leitura.data_hora = newDate.toISOString()
+        })
+    })
+    return dados
 }
--- src/routes/dados/+page.svelte
+++ src/routes/dados/+page.svelte
@@ -24,7 +24,6 @@
     }
 
     //-----------------------------------------------------------------------------
-
     let selectedDate: Date
     let dados: {
         leituras: Leitura
@@ -65,43 +64,6 @@
             })
     }
 
-    async function download(format: 'json' | 'csv') {
-        await fetch(
-            `/api/download?date=${selectedDate.toLocaleDateString(
-                'pt-BR'
-            )}&format=${format}`,
-            {
-                headers: {
-                    key: '123',
-                    format: format
-                }
-            }
-        ).then(async response => {
-            switch (response.status) {
-                case 200:
-                    const blob = await response.blob()
-                    const url = window.URL.createObjectURL(blob)
-                    const a = document.createElement('a')
-                    a.href = url
-                    a.download = `dados-${selectedDate.toLocaleDateString(
-                        'pt-BR'
-                    )}.${format}`
-                    document.body.appendChild(a)
-                    a.click()
-                    a.remove()
-                    break
-                default:
-                    modalStore.trigger({
-                        type: 'alert',
-                        title: 'Nada encontrado',
-                        body: 'Não foram encontradas leituras para a data selecionada',
-                        buttonTextCancel: 'Ok'
-                    })
-                    break
-            }
-        })
-    }
-
     onMount(() => {
         const datepickerEl = document.getElementById(
             'datepicker'
@@ -124,37 +86,117 @@
             }
         })
     })
+
+    async function handleSubmit(foda: Event) {
+        const form = foda.target as HTMLFormElement
+
+        const params = new URLSearchParams(
+            new FormData(form) as unknown as Record<string, string>
+        )
+
+        const url = new URL(form.action)
+        url.search = params.toString()
+
+        const res = await fetch(url, {
+            headers: {
+                key: '123'
+            }
+        })
+        if (res.ok) {
+            if (params.get('download')) {
+                const blob = await res.blob()
+                const url = window.URL.createObjectURL(blob)
+                const a = document.createElement('a')
+                a.href = url
+                a.download = `dados-${selectedDate.toLocaleDateString(
+                    'pt-BR'
+                )}.${params.get('download')}`
+                document.body.appendChild(a)
+                a.click()
+                a.remove()
+            } else dados = await res.json()
+        }
+
+        const inputs = document.querySelectorAll(
+            'input[name="download"]'
+        ) as NodeListOf<HTMLInputElement>
+        inputs.forEach(input => input.remove())
+    }
+
+    function handleDL(ev: Event) {
+        const target = ev.target as HTMLButtonElement
+        const format = target.textContent?.toLowerCase() as 'json' | 'csv'
+        const input = document.createElement('input')
+        input.type = 'hidden'
+        input.name = 'download'
+        input.value = format
+        target.form?.appendChild(input)
+    }
 </script>
 
 <svelte:head>
     <title>Dados - Labvirtual</title>
 </svelte:head>
 
-<section class="dark:text-white">
+<section
+    on:submit|preventDefault={handleSubmit}
+    class="dark:text-white"
+>
     <p>Selecione uma data para ver os dados</p>
 
-    <section class="flex gap-6 items-center my-4">
+    <form
+        action="/api/leituras"
+        class="flex gap-6 items-center my-4"
+    >
         <input
             id="datepicker"
             type="text"
             placeholder="Pick a date"
+            name="data"
+            required
         />
         <button
+            type="submit"
             title="Criar gráfico dos dados"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
-            on:click={() => fetchLeituras(selectedDate)}
         >
             <LineChart />
         </button>
 
         <button
+            type="button"
             title="Download dos dados"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
             use:popup={popupCombobox}
         >
             <FileDown />
         </button>
-    </section>
+        <div
+            class="card shadow-xl py-2"
+            data-popup="combobox"
+        >
+            <ul
+                class="space-y-1 mx-px flex flex-col w-32 dark:text-white text-black"
+            >
+                <li class="hover:bg-surface-200-700-token">
+                    <button
+                        on:click={handleDL}
+                        class="p-2 w-full"
+                    >
+                        JSON</button
+                    >
+                </li>
+                <li class="hover:bg-surface-200-700-token">
+                    <button
+                        class="p-2 w-full"
+                        on:click={handleDL}>CSV</button
+                    >
+                </li>
+            </ul>
+            <!-- Arrow -->
+            <div class="arrow bg-surface-100-800-token" />
+        </div>
+    </form>
 
     {#if dados}
         <div>
@@ -165,25 +207,3 @@
         </div>
     {/if}
 </section>
-
-<div
-    class="card shadow-xl py-2"
-    data-popup="combobox"
->
-    <ul class="space-y-1 mx-px flex flex-col w-32 dark:text-white text-black">
-        <li class="hover:bg-surface-200-700-token">
-            <button
-                class="p-2 w-full"
-                on:click={() => download('json')}>JSON</button
-            >
-        </li>
-        <li class="hover:bg-surface-200-700-token">
-            <button
-                class="p-2 w-full"
-                on:click={() => download('csv')}>CSV</button
-            >
-        </li>
-    </ul>
-    <!-- Arrow -->
-    <div class="arrow bg-surface-100-800-token" />
-</div>
