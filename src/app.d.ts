0e7579a0a8c6f599d383f57ee9e11353176f9f9c
Pedro Santiago <pedrossantiago1@gmail.com>
2024-03-07 20:12:41 -0300

    Re-início

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,36 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-    namespace App {
-        // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
-        // interface Platform {}
-    }
-
-    interface LeituraDB {
-        id_sensor_de_usuario: number
-        data_hora: Date
-        leitura: {
-            [key: string]: number
-        }
-    }
-
-    interface SensorInfo {
-        nome: string
-        descricao: string
-        dados_lidos: string
-    }
-
-    interface DadosSensor {
-        [key: string]: number[]
-    }
-
-    interface Leitura {
-        data: Date
-        horario: string[]
-        leituras: DadosSensor[]
-    }
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
 }
 
-export {}
+export {};
